9e3ec7098ca3bcbb1cf66fdedc4340d56d5515c0
WolfyDen <wolfyden@gmail.com>
2018-02-04 09:10:49 +0100

    Add project files.

---
+++ randomdatagen/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace randomdatagen
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.lbl_info = new System.Windows.Forms.Label();
+			this.rb_Student = new System.Windows.Forms.RadioButton();
+			this.rb_Teacher = new System.Windows.Forms.RadioButton();
+			this.btn_GenTables = new System.Windows.Forms.Button();
+			this.rb_AllData = new System.Windows.Forms.RadioButton();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Generate";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(91, 14);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(74, 20);
+			this.numericUpDown1.TabIndex = 1;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(171, 17);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(28, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "lines";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.richTextBox1.Location = new System.Drawing.Point(12, 50);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(904, 450);
+			this.richTextBox1.TabIndex = 3;
+			this.richTextBox1.Text = "";
+			// 
+			// button2
+			// 
+			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button2.Location = new System.Drawing.Point(12, 511);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Copy";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(93, 516);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(63, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Generated: ";
+			// 
+			// lbl_info
+			// 
+			this.lbl_info.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.lbl_info.AutoSize = true;
+			this.lbl_info.Location = new System.Drawing.Point(162, 516);
+			this.lbl_info.Name = "lbl_info";
+			this.lbl_info.Size = new System.Drawing.Size(13, 13);
+			this.lbl_info.TabIndex = 6;
+			this.lbl_info.Text = "0";
+			// 
+			// rb_Student
+			// 
+			this.rb_Student.AutoSize = true;
+			this.rb_Student.Checked = true;
+			this.rb_Student.Location = new System.Drawing.Point(222, 15);
+			this.rb_Student.Name = "rb_Student";
+			this.rb_Student.Size = new System.Drawing.Size(62, 17);
+			this.rb_Student.TabIndex = 7;
+			this.rb_Student.TabStop = true;
+			this.rb_Student.Text = "Student";
+			this.rb_Student.UseVisualStyleBackColor = true;
+			// 
+			// rb_Teacher
+			// 
+			this.rb_Teacher.AutoSize = true;
+			this.rb_Teacher.Location = new System.Drawing.Point(290, 15);
+			this.rb_Teacher.Name = "rb_Teacher";
+			this.rb_Teacher.Size = new System.Drawing.Size(65, 17);
+			this.rb_Teacher.TabIndex = 8;
+			this.rb_Teacher.Text = "Teacher";
+			this.rb_Teacher.UseVisualStyleBackColor = true;
+			// 
+			// btn_GenTables
+			// 
+			this.btn_GenTables.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btn_GenTables.Location = new System.Drawing.Point(773, 12);
+			this.btn_GenTables.Name = "btn_GenTables";
+			this.btn_GenTables.Size = new System.Drawing.Size(143, 23);
+			this.btn_GenTables.TabIndex = 9;
+			this.btn_GenTables.Text = "Generate Tables";
+			this.btn_GenTables.UseVisualStyleBackColor = true;
+			this.btn_GenTables.Click += new System.EventHandler(this.btn_GenTables_Click);
+			// 
+			// rb_AllData
+			// 
+			this.rb_AllData.AutoSize = true;
+			this.rb_AllData.Location = new System.Drawing.Point(361, 14);
+			this.rb_AllData.Name = "rb_AllData";
+			this.rb_AllData.Size = new System.Drawing.Size(62, 17);
+			this.rb_AllData.TabIndex = 10;
+			this.rb_AllData.Text = "All Data";
+			this.rb_AllData.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(928, 546);
+			this.Controls.Add(this.rb_AllData);
+			this.Controls.Add(this.btn_GenTables);
+			this.Controls.Add(this.rb_Teacher);
+			this.Controls.Add(this.rb_Student);
+			this.Controls.Add(this.lbl_info);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label lbl_info;
+		private System.Windows.Forms.RadioButton rb_Student;
+		private System.Windows.Forms.RadioButton rb_Teacher;
+		private System.Windows.Forms.Button btn_GenTables;
+		private System.Windows.Forms.RadioButton rb_AllData;
+	}
+}
+
---
+++ randomdatagen/Form1.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace randomdatagen
+{
+	public partial class Form1 : Form
+	{
+		List<int> tanárok = new List<int>() { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+
+		string[] uzenetek = new string[] { "Órán pékáruval dobálta társát. Megintem.", "Órán társa hátára minősíthetetlen feliratot ragasztott.", "Szünetben a fűtőtesten ült", "Órán bámulta a plafont.", "Körzőjével veszélyeztette társai testi épségét", "A gyerek az órán beszél, és állandóan jelentkezik", "A gyerek a szünetben az iskola keretein kívül játszott", "Tisztelt szülők! Fiuk az ebédlőben nemi szervét étkezési célokra ajánlotta fel, ezért megintem.", "Modortalan viselkedéséért megintem (A helyettesítőtanárt meglátva hangos öklendezésbe kezdett.)", "Órán rendetlen voltam, tiszteletlenül beszéltem", "Elkobozta társa tízóraiját", "Kedves Szülő! Csúnya beszédért osztályfőnöki figyelmeztetésben részesítem!", "T. Szülők! A tanórára gyíkot hozott be az udvarról, ami a tanteremben 'kiszabadult' a táskájából! Kérném elbeszélgetni a gyermekkel! [Ezzel ugyanis az órát zavarta]", "Tisztelt Szülők! Fiúk német órán több alkalommal hozzáért a tanárnőhöz és erotikus megjegyzéseket tett. Ezért osztályfőnöki rovóban részesítem. Kérem beszélgessenek el vele erről és értessék meg vele, hogy ne fogdossa a tanárnőt", "Tisztelt szülők! Fiuk az órákon rendszeresen gusztustalankodik, és kulturálatlan viselkedését hőstettként éli meg, holott zavarja társait, az órát. Kérem figyelmeztessék a közösségi élet általános szabályaira, mert ennek hiányában nem méltó az iskolánkhoz.", "Tisztelt Szülők! Lányuk az utcasarkon egy szál pólóban cigizik. Meg fog fázni.", "Az osztályfőnöki dicséretet még mindig nem íratta alá. Megintem.", "Kémia órán a kísérleti anyagot elfogyasztotta.", "Gyermeke az órámon írószer nélkül akart írni.", "Kémiaóra folyamán padtársával zümmögött és brummogott.", "Gyermekük óra alatt orvul elfogyasztotta padtársa uzsonnáját.", "Két ellenőrzője van. Én mosom kezeimet. – Osztályfőnök.", "Fia olvashatatlanul ír. Bár párszor még így jár jobban.", "Kesztyűben ül az órán, mert fázik a keze és nem írja a szavakat.", "Óra alatt kukorékolt!", "A gyerek a szünetben az iskola keretein kívül játszott.", "A gyerek az órán beszél, és állandóan jelentkezik.", "Körzőjével veszélyeztette társai testi épségét", "Elkobozta társa tízóraiját", "Fia óra alatt a pad sarkával jéghokisat játszott a terem körül, eközben MÁV bemondásokat tartott, melyben a rólam elnevezett szerelvény közeledtére figyelmeztetett", "Kedves szülő, ha megígéri, hogy nem hiszi el mindazt amit a fia az iskoláról mesél, én cserébe nem hiszem el mindazt, amit ő az otthoni eseményekről mond.", "Gyermekük matekórán a zsiráf hangját utánozta.", "Szellemessége és feltűnési viszketegsége a pofátlanság határait súrolja.", "Fia a szünetben sztrájkot szervezett. Letörtük.", "Nem elég, hogy a szünetekben állandóan krétacsatákat vív osztálytársaival, ráadásul mindig veszít.", "Tűvel látta el barátait, hogy osztálytársait szurkálják.", "Gyermeke azt mondta, hogy szerinte jó gondolat lenne az egyik osztálytársát bántalmazni, ezért figyelmeztetem.", "T. Szülő, a gyereke állandóan tőlem kér tollat. Lássa el otthon írószerrel.", "Tájékoztatom, hogy gyerekének egy ideje átható kénszaga van." };
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (rb_Student.Checked)
+			{
+				genStudents((int)numericUpDown1.Value);
+			}
+			else if (rb_Teacher.Checked)
+			{
+				genTeachers((int)numericUpDown1.Value);
+			}
+			else if (rb_AllData.Checked)
+			{
+				generateData();
+			}
+		}
+
+		public void generateData()
+		{
+			int teach = 20;
+			int stud = 200;
+
+			generateTables();
+			genTeachers(teach);
+			genStudents(stud);
+
+			lbl_info.Text = "Generated all data. Table structure, base data, " + teach + " teachers and " + stud + " students.";
+		}
+
+		public void genTeachers(int val)
+		{
+			tanárok.Clear();
+			if(val < 10) { val = 10; }
+
+			List<int> ofők = new List<int>();
+
+			for (int i = 0; i < val; i++)
+			{
+				Thread.Sleep(10);
+				int gender = genRandom(0,2,3+i);
+				string name = names.getName(gender);
+				Thread.Sleep(5);
+				int osztalyfonok = i < 10 ? 1 : 0;
+
+				tanárok.Add(i + 1);
+
+				if(osztalyfonok == 1) { ofők.Add(i); }
+
+				richTextBox1.Text += string.Format("INSERT INTO tanarok (id,nev,nem,osztaly_fonok) VALUES ({0},'{1}',{2},{3}); \r\n",
+					i + 1,
+					name,
+					gender,
+					osztalyfonok
+					);
+
+				lbl_info.Text = (i + 1) + " teachers";
+				lbl_info.Refresh();
+			}
+
+			List<int> otermek = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+			for(int i = 0; i < 10; i++)
+			{
+				int t = genRandom(0, otermek.Count);
+				int teremRand = otermek[t];
+				otermek.Remove(teremRand);
+
+				richTextBox1.Text += string.Format("INSERT INTO osztalyok (osztaly_fonok,osztaly_terem,megjegyzes) VALUES ({0},{1},'{2}'); \r\n",
+					ofők[i],
+					teremRand,
+					"-"
+					);
+			}
+		}
+
+		public void genStudents(int val)
+		{
+			for (int i = 0; i < val; i++)
+			{
+				Thread.Sleep(2);
+				int gender = genRandom(0, 2);
+				string[] n = names.getNames(gender);
+				string originalName = "";
+				Thread.Sleep(2);
+				if (genRandom(0,1001) > 900) { originalName = n[0].Split(' ')[0] + " " + names.getName(gender).Split(' ')[1]; }
+				Thread.Sleep(2);
+				int szul_ev = genRandom(1990, 2000, 10);
+				int szul_ho = genRandom(1, 13, 10);
+				int szul_nap = genRandom(1, 31, 20); 
+				int osztaly = 2000 - szul_ev;
+				Thread.Sleep(2);
+				string szletesi_hely = names.varosok[genRandom(0, names.varosok.Length, 100)];
+				int magaviselet = genRandom(1, 6, 999);
+
+				//todo: fix rokon
+
+				int rokon = genRandom(0, 1001);
+				string apa = "";
+
+				if(genRandom(0, 1001) > 300) { apa = n[0].Split(' ')[0] + " " + names.fiunevek[genRandom(0, names.fiunevek.Length, 865)]; }
+				List<string> testverek = new List<string>();
+
+				int numOfSiblings = (genRandom(-1,6,1) + genRandom(-1, 6, 10) + genRandom(-1, 6, 100)) / 3;
+				if(numOfSiblings > 0)
+				{
+					for(int ii = 0; ii < numOfSiblings; ii++)
+					{
+						Thread.Sleep(10);
+						testverek.Add(n[0].Split(' ')[0] + " " + (genRandom(0, 2, 52) == 0 ? names.fiunevek[genRandom(0, names.fiunevek.Length, Environment.TickCount + 100)] : names.lanynevek[genRandom(0, names.lanynevek.Length, 150)]));
+					}
+				}
+
+				
+				List<string> nagyszulok = new List<string>();
+				Thread.Sleep(10);
+				if (genRandom(0, 11, 11) > 5) { nagyszulok.Add(n[0].Split(' ')[0] + " " + names.fiunevek[genRandom(0, names.fiunevek.Length, 20776)]); } //Apai nagyapa
+				Thread.Sleep(10);
+				if (genRandom(0, 11, 12) > 5) { nagyszulok.Add(names.getName(1, "")); } // Apai nagyanya
+				Thread.Sleep(10);
+				if (genRandom(0, 11, 13) > 5) { nagyszulok.Add(n[1].Split(' ')[0] + " " + names.fiunevek[genRandom(0, names.fiunevek.Length, 23497)]); } //Anyai nagyapa
+				Thread.Sleep(10);
+				if (genRandom(0, 11, 14) > 5) { nagyszulok.Add(names.getName(1, "")); } //Anyai nagyanya
+
+
+				richTextBox1.Text += string.Format("INSERT INTO diakok (id,nev,csalad_nev,uto_nev,szuletesi_nev,nem,anyja_neve,szuletesi_datum,szuletesi_hely,szig_szam,osztaly,magaviselet) VALUES ({11},'{0}','{1}','{2}','{3}',{4},'{5}','{6}','{7}','{8}',{9},{10}); \r\n",
+					n[0],
+					n[0].Split(' ')[0],
+					n[0].Split(' ')[1],
+					originalName,
+					(gender + 1),
+					n[1],
+					szul_ev + "." + szul_ho + "." + szul_nap + ".",
+					szletesi_hely,
+					generateszigszam(),
+					osztaly,
+					magaviselet,
+					i
+					);
+
+				if (apa != "") { richTextBox1.Text += string.Format("INSERT INTO csaladtagok (diak, nev, kapcsolat) VALUES ({0},'{1}',{2});\r\n", i, apa, "1"); }
+				richTextBox1.Text += string.Format("INSERT INTO csaladtagok (diak, nev, kapcsolat) VALUES ({0},'{1}',{2});\r\n", i, n[1], "1");
+
+				foreach (string s in testverek)
+				{
+					richTextBox1.Text += string.Format("INSERT INTO csaladtagok (diak, nev, kapcsolat) VALUES ({0},'{1}',{2}); -- testver \r\n", i, s, "3");
+				}
+
+				foreach (string s in nagyszulok)
+				{
+					richTextBox1.Text += string.Format("INSERT INTO csaladtagok (diak, nev, kapcsolat) VALUES ({0},'{1}',{2}); -- nagyszulo \r\n", i, s, "5");
+				}
+
+				int jegyek = genRandom(3, 20, 9872345);
+				for(int j = 0; j < jegyek; j++)
+				{
+					Thread.Sleep(5);
+					int ora = genRandom(1, 13, 888);
+					Thread.Sleep(5);
+					int jegy = genRandom(1, 6, 18181 + j);
+					Thread.Sleep(5);
+					int tanar = tanárok[genRandom(0, tanárok.Count,1214)];
+
+					richTextBox1.Text += string.Format("INSERT INTO naplo_bejegyzesek (ora,tanar,diak, jegy) VALUES ({0},{1},{2},{3}); \r\n", ora, tanar, i, jegy);
+				}
+
+				if(genRandom(0,1001) > 800)
+				{
+					Thread.Sleep(5);
+					int tanar = tanárok[genRandom(0, tanárok.Count, 1214)];
+					string uzenet = string.Format("INSERT INTO uzenetek (tanar,diak,uzenet) VALUES ({0},{1},'{2}'); -- üzenet\r\n", tanar, i, uzenetek[genRandom(0, uzenetek.Length)]);
+					richTextBox1.Text += uzenet;
+				}
+
+				lbl_info.Text = (i + 1) + " students";
+				lbl_info.Refresh();
+			}
+
+			
+		}
+
+		public int genRandom(int min, int max) { return genRandom(min, max, 0); }
+		public int genRandom(int min, int max, int seed)
+		{
+			return new Random(Environment.TickCount + seed).Next(min, max);
+		}
+
+		public string generateszigszam()
+		{
+			Thread.Sleep(2);
+			return new Random().Next(100000,999999).ToString() + names.betuk[new Random(Environment.TickCount - 10009).Next(0,names.betuk.Length)] + names.betuk[new Random(Environment.TickCount).Next(0, names.betuk.Length)] + "";
+		}
+
+		private void btn_GenTables_Click(object sender, EventArgs e)
+		{
+			generateTables();
+		}
+
+		public void generateTables()
+		{
+			lbl_info.Text = "Table Structure and base data.";
+
+			richTextBox1.Text = @"
+CREATE TABLE `terem_allapot_ref` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`megnevezes`	TEXT NOT NULL,
+	`leiras`	TEXT DEFAULT 'Nincs Leírás'
+);
+CREATE TABLE `orak` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`ora_neve`	TEXT NOT NULL
+);
+CREATE TABLE magaviselet_tipus (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`megnevezes`	INTEGER,
+	`leiras`	INTEGER
+);
+CREATE TABLE csaladtagok (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`diak`	INTEGER,
+	`nev`	TEXT,
+	`kapcsolat`	INTEGER,
+	FOREIGN KEY(`diak`) REFERENCES `diakok`(`id`),
+	FOREIGN KEY(`kapcsolat`) REFERENCES rokon_tipus(id)
+);
+CREATE TABLE `rokon_tipus` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`kapcsolat`	TEXT
+);
+CREATE TABLE naplo_bejegyzesek (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`ora`	INTEGER,
+	`tanar`	INTEGER,
+	`diak`	INTEGER,
+	`jegy`	INTEGER NOT NULL DEFAULT 3,
+	`megjegyzes`	INTEGER,
+	FOREIGN KEY(`ora`) REFERENCES `orak`(`id`),
+	FOREIGN KEY(`tanar`) REFERENCES `tanarok`(`id`),
+	FOREIGN KEY(`diak`) REFERENCES `diakok`(`id`)
+);
+CREATE TABLE osztalytermek (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`emelet`	INTEGER NOT NULL,
+	`ajto`	INTEGER NOT NULL,
+	`allapot`	INTEGER NOT NULL,
+	`megjegyzes`	TEXT,
+	FOREIGN KEY(`allapot`) REFERENCES `terem_allapot_ref`(`id`)
+);
+CREATE TABLE uzenetek (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`tanar`	INTEGER,
+	`diak`	INTEGER,
+	`uzenet`	TEXT,
+	FOREIGN KEY(`tanar`) REFERENCES `tanarok`(`id`)
+);
+CREATE TABLE `nemek` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`nem`	TEXT
+);
+CREATE TABLE tanarok (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`nev`	TEXT NOT NULL,
+	`nem`	INTEGER NOT NULL DEFAULT 0,
+	`osztaly_fonok`	INTEGER NOT NULL DEFAULT 0,
+	`megjegyzes`	TEXT,
+	FOREIGN KEY(`nem`) REFERENCES nemek(id)
+);
+CREATE TABLE diakok (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`nev`	TEXT NOT NULL,
+	`csalad_nev`	TEXT,
+	`uto_nev`	TEXT,
+	`szuletesi_nev`	TEXT,
+	`nem`	INTEGER,
+	`anyja_neve`	TEXT NOT NULL,
+	`szuletesi_datum`	TEXT NOT NULL,
+	`szuletesi_hely`	TEXT NOT NULL,
+	`szig_szam`	TEXT NOT NULL,
+	`osztaly`	INTEGER NOT NULL,
+	`magaviselet`	INTEGER,
+	`megjegyzes`	TEXT,
+	FOREIGN KEY(`nem`) REFERENCES `nemek`(`id`),
+	FOREIGN KEY(`osztaly`) REFERENCES `osztalyok`(`id`),
+	FOREIGN KEY(`magaviselet`) REFERENCES `magaviselet_tipus`(`id`)
+);
+CREATE TABLE osztalyok (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`osztaly_fonok`	INTEGER NOT NULL,
+	`osztaly_terem`	INTEGER NOT NULL,
+	`megjegyzes`	TEXT,
+	FOREIGN KEY(`osztaly_fonok`) REFERENCES tanarok(id),
+	FOREIGN KEY(`osztaly_terem`) REFERENCES `osztalytermek`(`id`)
+);
+INSERT INTO terem_allapot_ref (id,megnevezes, leiras) VALUES (1,'Kíváló', 'A terem minden elvárásnak megfelel'); 
+INSERT INTO terem_allapot_ref (id,megnevezes,leiras)  VALUES (2,'Hiányos', 'A terem jó állapotban van, de hiányos a felszereltsége');
+INSERT INTO terem_allapot_ref (id,megnevezes,leiras)  VALUES (3,'Sérült', 'A teremben elhelyezett bútorok némelyike sérültm károsodott');
+INSERT INTO terem_allapot_ref (id,megnevezes,leiras)  VALUES (4,'Tanításra alkalmatlan', 'A terem olyan mértékben károsodott, hogy az egészségre káros, vagy más okból a tanítás folytatására nem alkalmas');
+INSERT INTO rokon_tipus(id, kapcsolat) VALUES(1, 'Édesszülő'); 
+INSERT INTO rokon_tipus (id,kapcsolat) VALUES (2, 'Mostohaszülő');
+INSERT INTO rokon_tipus (id,kapcsolat) VALUES (3, 'Édestestvér');
+INSERT INTO rokon_tipus (id,kapcsolat) VALUES (4, 'Mostohatestvér');
+INSERT INTO rokon_tipus (id,kapcsolat) VALUES (5, 'Nagyszülő');
+INSERT INTO rokon_tipus (id,kapcsolat) VALUES (6, 'Egyéb');
+INSERT INTO rokon_tipus (id,kapcsolat) VALUES (7, 'Nincs Kapcsolat');
+INSERT INTO orak (id,ora_neve) VALUES (1, 'Magyar Nyelv');
+INSERT INTO orak (id,ora_neve) VALUES (2, 'Irodalom');
+INSERT INTO orak (id,ora_neve) VALUES (3, 'Matematika');
+INSERT INTO orak (id,ora_neve) VALUES (4, 'Történelem');
+INSERT INTO orak (id,ora_neve) VALUES (5, 'Kémia');
+INSERT INTO orak (id,ora_neve) VALUES (6, 'Fizika');
+INSERT INTO orak (id,ora_neve) VALUES (7, 'Angol Nyelv');
+INSERT INTO orak (id,ora_neve) VALUES (8, 'Német Nyelv');
+INSERT INTO orak (id,ora_neve) VALUES (9, 'Informatika');
+INSERT INTO orak (id,ora_neve) VALUES (10, 'Rajz');
+INSERT INTO orak (id,ora_neve) VALUES (11, 'Zene');
+INSERT INTO orak (id,ora_neve) VALUES (12, 'Biológia');
+INSERT INTO osztalytermek (id,emelet,ajto,allapot,megjegyzes) VALUES (1, 0, 1, 2, '-');
+INSERT INTO osztalytermek (id,emelet,ajto,allapot,megjegyzes) VALUES (2, 0, 2, 1, '-');
+INSERT INTO osztalytermek (id,emelet,ajto,allapot,megjegyzes) VALUES (3, 0, 3, 1, '-');
+INSERT INTO osztalytermek (id,emelet,ajto,allapot,megjegyzes) VALUES (4, 1, 11, 3, '-');
+INSERT INTO osztalytermek (id,emelet,ajto,allapot,megjegyzes) VALUES (5, 1, 12, 2, '-');
+INSERT INTO osztalytermek (id,emelet,ajto,allapot,megjegyzes) VALUES (6, 1, 13, 1, '-');
+INSERT INTO osztalytermek (id,emelet,ajto,allapot,megjegyzes) VALUES (7, 1, 14, 2, '-');
+INSERT INTO osztalytermek (id,emelet,ajto,allapot,megjegyzes) VALUES (8, 2, 21, 2, '-');
+INSERT INTO osztalytermek (id,emelet,ajto,allapot,megjegyzes) VALUES (9, 2, 22, 1, '-');
+INSERT INTO osztalytermek (id,emelet,ajto,allapot,megjegyzes) VALUES (10, 2, 23, 1, '-');
+INSERT INTO nemek(id, nem) VALUES(1, 'Férfi'); 
+INSERT INTO nemek (id,nem) VALUES (2, 'Nő');";
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			Clipboard.SetText(richTextBox1.Text);
+			lbl_info.Text = "Generated data copied to clipboard!";
+		}
+	}
+}
